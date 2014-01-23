15f1df0d34832f6b39fbb4d71f57102aa5925697
Dave Griffiths <dave@fo.am>
2014-01-23 14:44:30 +0000

    test fixes

--- tests/osctest.py
+++ tests/osctest.py
@@ -85,6 +85,6 @@ code = ["""
 while 1:
 
     print("sending")
-    #osc.Message("/eval",[code1]).sendto("192.168.1.171",8000)
-    osc.Message("/eval",[choice(code)]).sendlocal(8000)
-    time.sleep(5)
+    osc.Message("/eval",[choice(code)]).sendto("192.168.1.192",8000)
+    #osc.Message("/eval",[choice(code)]).sendlocal(8000)
+    time.sleep(60)
